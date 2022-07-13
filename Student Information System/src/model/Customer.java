25c032d8e0c242e920b2715e44de1cec3f0f0a5c
91966 <91966@Priya>
2022-07-13 12:25:49 +0530

    Added Customer.java file

---
+++ Student Information System/src/model/Customer.java
@@ -0,0 +1,25 @@
+package model;
+
+public class Customer {
+	int customerId;
+	String customerName;
+	public Customer(int customerId, String customerName) {
+		super();
+		this.customerId = customerId;
+		this.customerName = customerName;
+	}
+	public int getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	
+
+}
