5985e6df2ec45e2a46ce95668b14cf17dbc539c7
91966 <91966@Priya>
2022-07-13 12:59:37 +0530

    Modified

--- Student Information System/src/model/Customer.java
+++ Student Information System/src/model/Customer.java
@@ -3,6 +3,13 @@ package model;
 public class Customer {
 	int customerId;
 	String customerName;
+	int customerAge;
+	public int getCustomerAge() {
+		return customerAge;
+	}
+	public void setCustomerAge(int customerAge) {
+		this.customerAge = customerAge;
+	}
 	public Customer(int customerId, String customerName) {
 		super();
 		this.customerId = customerId;
